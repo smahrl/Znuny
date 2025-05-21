24654bb6d1dc27db21e8efa7d556bdb8befeacc1
smahrl <smahrl@gmail.com>
2025-05-21 23:06:02 +0200

    Fixed Bug - TicketZoom::TicketInformation: Incorrect state filtering for CustomerIDTickets, now counts only “open” states.
    Fixed Bug - TicketZoom::TicketInformation: Incorrect state filtering for CustomerIDTickets, now counts only “open” and “new” states.

--- Kernel/Output/HTML/TicketZoom/TicketInformation.pm
+++ Kernel/Output/HTML/TicketZoom/TicketInformation.pm
@@ -146,6 +146,7 @@ sub Run {
                 Result     => 'COUNT',
                 Permission => 'ro',
                 UserID     => $Self->{UserID},
+                StateType  => [ 'open'],
             );
             $LayoutObject->Block(
                 Name => 'CustomerIDTickets',
